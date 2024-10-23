95d838f7d3bf52b5b2a3e211e55c5946d232ef36
Meher Labidi <183096701+meher-labidi@users.noreply.github.com>
2024-10-23 13:29:58 +0100

    cafe_data.py

--- cafe_data.py
+++ cafe_data.py
@@ -1,16 +1,16 @@
 import csv
 import os
 
-# default data file
+
 DEFAULT_CSV_FILE = "cafe-data.csv"
-# default header, in case there is no CSV file
+
 DEFAULT_HEADER = ["Cafe Name", "Location", "Open", "Close", "Coffee", "Wifi", "Power"]
 
 
 class CafeManager:
 
     def __init__(self, data_file=DEFAULT_CSV_FILE):
-        # use the default CSV file, unless a different one is specified
+
         self._data_file = data_file
 
     def get_cafes(self):
@@ -21,20 +21,19 @@ class CafeManager:
                 cafe_header = []
                 cafe_list = []
                 for row in reader:
-                    # keep the header separate from the cafes
+                    
                     if len(cafe_header) == 0:
                         cafe_header = row
                     else:
                         cafe_list.append(row)
         except FileNotFoundError:
-            # return the default header and an empty list of cafes
+         
             cafe_header = DEFAULT_HEADER
             cafe_list = []
         return cafe_header, cafe_list
 
     def add_cafe(self, new_entry):
         """Takes a LIST and adds it as new row into the data file."""
-        # in case there is no CSV file, create a new one with just the default header
         if not self._data_file:
             with open(file=self._data_file, mode="w", encoding="utf-8", newline='') as f:
                 header = ','.join(DEFAULT_HEADER)
